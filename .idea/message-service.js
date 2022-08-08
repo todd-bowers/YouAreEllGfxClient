f4b137e6ddb0f2c6e5f597025ee30de4a5da6055
todd-bowers <toddbows@gmail.com>
2022-08-07 22:08:18 -0400

    just need to debug

--- .idea/message-service.js
+++ .idea/message-service.js
@@ -1,17 +1,43 @@
 export default class MessageService {
 
     getAllMessages() {
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
+        new Promise(function (resolve, reject) {
         request.onload = function() {
             if (request.status >= 200 && request.status < 300) {
-                console.log(JSON.parse(request.responseText));
+                const threads = JSON.parse(request.responseText);
+                resolve(threads);
             } else {
-                console.log('Error: ' + request.status);
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
             }
         };
+
         request.open("GET", "http://zipcode.rocks:8085/messages");
 
         request.send();
+    })
+}
+    createNewMessage(message) {
+        const request = new XMLHttpRequest();
+
+        return new Promise(function (resolve, reject) {
+            request.onload = function () {
+                if (request.status >= 200 && request.status < 300) {
+                resolve(JSON.parse(request.responseText));
+                } else {
+                    reject({
+                    status: request.status,
+                    statusText: request.statusText
+                    });
+                }
+            };
+            request.open("POST", 'http://zipcode.rocks:8085/ids/${message.fromid}/messages');
+
+            request.send(JSON.stringify(message));
+        });
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 import MessageService from "./message-service.js";
 
 let userId = "";
-const messageService = new MessageService();
+const messageService = new MessageService(userId);
 
 window.addEventListener("load", function () {
     document.getElementById("greeting").innerHTML = 'Welcome ${userID}!';
-    messageService.getAllMessages();
+    messageService.getAllMessages()
+        .then(successCallback, errorCallback);
+
+    function successCallback(response) {
+        //should this be called populateThreads?
+        populateMessages(response);
+    }
+
+    function errorCallback(response) {
+        console.log(response);
+    }
 });
+
+//should this be called populateThread?
+function populateMessages(messages) {
+    messages.forEach(message => {
+        addMessageToThread(message);
+    })
+}
+
+function createFormListener() {
+    const form = document.getElementById("new-message-form");
+
+    form.onsubmit = function (event) {
+        event.preventDefault();
+
+        const data = {
+            fromid: userId,
+            message: form.message.value
+        };
+
+        messageService.createNewMessage(data)
+            .then(successCallback, errorCallback);
+
+        function successCallback(response) {
+            addMessageToThread(response);
+        }
+
+        function errorCallback(response) {
+            console.log(response);
+        }
+    }
+};
+
+function addMessageToThread(message) {
+    const messageListItem = document.createElement("LI");
+    const userIDHeading = document.createElement("h3");
+    const messageParagraph = document.createElement("p");
+    const messageContent = document.createTextNode(message.message);
+    const userIDContent = document.createTextNode(message.fromid);
+    userIDHeading.appendChild(userIDContent);
+    messageParagraph.appendChild(messageContent);
+    messageListItem
+        .appendChild(userIDHeading)
+        .appendChild(messageParagraph);
+    document.getElementById("message-list").appendChild(messageListItem);
+}
